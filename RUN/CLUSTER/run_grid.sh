22bf415420aaa6c5e94a0c2a57a828392a2ea4f2
Noam Wunch <noamwunch1@gmail.com>
2020-12-23 15:55:36 +0200

    add memory limtations to cluster run script

--- RUN/CLUSTER/run_grid.sh
+++ RUN/CLUSTER/run_grid.sh
@@ -1,4 +1,4 @@
 ### Run with:
 ### qsub -q kats.q -N <folder_name> -t <lines in folder_name/argseed.txt> array_job.sh
 
-qsub -q kats.q -N example_grid -t 1-2 -l mem_free=50G array_job.sh
+qsub -q kats.q -N example_grid -t 1-2 -l mem_free=50G -pe shared 10 array_job.sh
