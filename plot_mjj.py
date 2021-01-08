55acad1e1aa59b8fad83fa3a090e2d1abed77903
Noam Wunch <noamwunch1@gmail.com>
2021-01-08 21:39:25 +0200

    ?

--- plot_mjj.py
+++ plot_mjj.py
@@ -6,7 +6,7 @@ from matplotlib import pyplot as plt
 from UTILS.utils import evs_txt2jets_df as load_data
 
 plot_path = "RESULTS/mjj"
-Path(plot_path).mkdir(parents=True)
+Path(plot_path).mkdir(parents=True, exist_ok=True)
 
 S_path = "~/semisup_evs/rinv_0.0_mjj_500"
 _, S_j1_df = load_data(S_path, n_ev=10000)
