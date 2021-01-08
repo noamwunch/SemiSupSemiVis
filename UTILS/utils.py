cebc50238ee23de95f6e84d4d69f4b836f51712f
Noam Wunch <noamwunch1@gmail.com>
2021-01-08 22:03:36 +0200

    ?

--- UTILS/utils.py
+++ UTILS/utils.py
@@ -54,7 +54,7 @@ def evs_txt2jets_df(events_dir_path, n_ev=int(1e6), sort="PT"):
     ev_num = 0
     pathlist = Path(events_dir_path).glob('**/*root.txt')
     print(list(pathlist))
-    print(len(pathlist))
+    print(len(list(pathlist)))
     for events_path in list(pathlist):
         print("looping over pathlist")
         if ev_num > n_ev:
