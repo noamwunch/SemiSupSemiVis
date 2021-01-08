fe263808dbd5c929d4c80e363f2135488de10a6d
Noam Wunch <noamwunch1@gmail.com>
2021-01-08 22:16:37 +0200

    ?

--- plot_mjj.py
+++ plot_mjj.py
@@ -9,10 +9,11 @@ plot_path = "RESULTS/mjj"
 Path(plot_path).mkdir(parents=True, exist_ok=True)
 
 S_path = "/gpfs0/kats/users/wunch/semisup_evs/rinv_0.0_mjj_500"
-_, S_j1_df = load_data(S_path, n_ev=10000)
+_, S_j1_df = load_data(S_path)
 
 S_ev_df = S_j1_df[["MET", "Mjj"]]
 
 plt.figure()
-plt.hist(np.sqrt(S_ev_df.Mjj), bins=50)
+plt.hist(np.sqrt(S_ev_df.Mjj), bins=100, density=True, range=[50, 2000])
+plt.xlabel('M_jj [GeV]')
 plt.savefig(plot_path+"/mjj_signal_dist.png")
