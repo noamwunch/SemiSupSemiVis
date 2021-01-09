3b3c27596e6974b0343b4db99d8813b1d8ba89b8
Noam Wunch <noamwunch1@gmail.com>
2021-01-09 15:39:31 +0200

    ?

--- GENEV/root_to_txt/arrjob_root2txt.sh
+++ GENEV/root_to_txt/arrjob_root2txt.sh
@@ -13,7 +13,7 @@
 macro_dir=$(pwd)
 root_program_dir=/gpfs0/kats/projects/Delphes-3.4.2
 
-root_file_dir="/gpfs0/kats/users/wunch/semisup_evs/bkg"
+root_file_dir="/gpfs0/kats/users/wunch/semisup_evs/sig_rinv_0.00_mjj_500"
 
 inp_file="$root_file_dir/$SGE_TASK_ID.root"
 
