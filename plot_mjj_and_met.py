4b8de3723ba3899aaf4a93b79c5cd2103062bb7a
Noam Wunch <noamwunch1@gmail.com>
2021-01-09 17:26:10 +0200

    ?

--- plot_mjj_and_met.py
+++ plot_mjj_and_met.py
@@ -6,6 +6,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 from UTILS.utils import evs_txt2jets_df as load_data
+from semisup import combine_SB
 
 plot_path = "RESULTS/mjj"
 Path(plot_path).mkdir(parents=True, exist_ok=True)
@@ -15,11 +16,26 @@ S_rinv0_path = "/gpfs0/kats/users/wunch/semisup_evs/sig_rinv_0.00_mjj_500"
 S_rinv1_path = "/gpfs0/kats/users/wunch/semisup_evs/sig_rinv_0.25_mjj_500"
 S_rinv2_path = "/gpfs0/kats/users/wunch/semisup_evs/sig_rinv_0.50_mjj_500"
 
+if sys.argv[1]=="combined":
+    sig_frac = float(sys.argv[2])
+    N = 80000
+    S_path = S_rinv0_path
+    hist_args = {'bins': 100, 'density': True, 'histtype': 'step'}
+
+    j1_df, _, _ = combine_SB(B_path, S_path, N, sig_frac)
+    ev_df = j1_df[["MET", "Mjj"]]
+
+    plt.figure()
+    plt.title(f'{N} events with {sig_frac*100}% signal fraction (rinv=0)')
+    plt.xlabel('$M_{jj}$ [GeV]')
+    plt.savefig(plot_path+f"/mjj_dist_sig_frac_{sig_frac}_rinv_0.00.png")
+    plt.hist(np.sqrt(ev_df.Mjj), range=[50, 1250], **hist_args)
 
 if sys.argv[1]=="all":
     paths = [B_path, S_rinv0_path, S_rinv1_path, S_rinv2_path]
     names = ['Background (b b~)', 'Signal rinv=0.00', 'Signal rinv=0.25', 'Signal rinv=0.50']
     hist_args = {'bins': 100, 'density': True, 'histtype': 'step'}
+
     met_fig = plt.figure()
     mjj_fig = plt.figure()
     for path in paths:
