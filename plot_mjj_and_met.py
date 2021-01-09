55ced7ecaf408450c223cedd9944da504e8955d7
Noam Wunch <noamwunch1@gmail.com>
2021-01-09 16:39:47 +0200

    ?

--- plot_mjj_and_met.py
+++ plot_mjj_and_met.py
@@ -15,7 +15,32 @@ S_rinv0_path = "/gpfs0/kats/users/wunch/semisup_evs/sig_rinv_0.00_mjj_500"
 S_rinv1_path = "/gpfs0/kats/users/wunch/semisup_evs/sig_rinv_0.25_mjj_500"
 S_rinv2_path = "/gpfs0/kats/users/wunch/semisup_evs/sig_rinv_0.50_mjj_500"
 
-print(sys.argv[0])
+
+if sys.argv[1]=="all":
+    paths = [B_path, S_rinv0_path, S_rinv1_path, S_rinv2_path]
+    names = ['Background (b b~)', 'Signal rinv=0.00', 'Signal rinv=0.25', 'Signal rinv=0.50']
+    hist_args = {'bins': 100, 'density': True, 'histtype': 'step'}
+    met_fig = plt.figure()
+    mjj_fig = plt.figure()
+    for path in paths:
+        _, j1_df = load_data(B_path)
+        ev_df = j1_df[["MET", "Mjj"]]
+
+        plt.figure(mjj_fig.number)
+        plt.hist(np.sqrt(ev_df.Mjj), **hist_args)
+
+        plt.figure(met_fig.number)
+        plt.hist(np.sqrt(ev_df.MET), **hist_args)
+
+    plt.figure(mjj_fig.number)
+    plt.xlabel('$M_{jj}$ [GeV]')
+    plt.savefig(plot_path+"/mjj_dist.png")
+    plt.legend(names)
+
+    plt.figure(met_fig.number)
+    plt.xlabel('MET [GeV]')
+    plt.savefig(plot_path+"/met_dist.png")
+    plt.legend(names)
 
 ######Background#######
 if sys.argv[1]=="0":
@@ -24,13 +49,9 @@ if sys.argv[1]=="0":
     print(f"Number of bkg events: {len(B_ev_df)}")
 
     plt.figure()
-    plt.hist(np.sqrt(B_ev_df.Mjj), bins=100, density=True)  # , range=[50, 1500])
+    plt.hist(np.sqrt(B_ev_df.Mjj), bins=100, density=True)
     plt.xlabel('$M_{jj}$ [GeV]')
     plt.savefig(plot_path+"/mjj_bkg_dist.png")
-    plt.text(0.9, 0.9, f'N = {len(B_ev_df)}',
-             horizontalalignment='center',
-             verticalalignment='center',
-             transform=plt.gca().transAxes)
 
     plt.figure()
     plt.hist(np.sqrt(B_ev_df.MET), bins=100, density=True)
