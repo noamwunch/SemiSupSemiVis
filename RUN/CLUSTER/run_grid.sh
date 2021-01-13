820e2a1ba59f1f48eab330bdba83e79be6a73fd8
Noam Wunch <noamwunch1@gmail.com>
2021-01-13 21:13:59 +0200

    ?

--- RUN/CLUSTER/run_grid.sh
+++ RUN/CLUSTER/run_grid.sh
@@ -1,5 +1,5 @@
 ### Run with:
 ### qsub -q kats.q -N <folder_name> -t <lines in folder_name/argseed.txt> array_job.sh
-rm ./$1/std*
-nlines=$(($(< "./$1/argseed.txt" wc -l)))
+rm -f ./$1/std*
+nlines=$(($(< "./$1/argseed.txt" wc -l)+1))
 qsub -q kats.q -N $1 -t 1-$nlines -l mem_free=50G -pe shared 10 array_job.sh
