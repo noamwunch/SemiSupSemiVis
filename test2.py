c0128db03111fe0cda82d7f8f239ff218cc9a57e
Noam Wunch <noamwunch1@gmail.com>
2021-01-14 17:27:56 +0200

    ?

--- test2.py
+++ test2.py
@@ -11,32 +11,28 @@ Path(plot_path).mkdir(parents=True, exist_ok=True)
 
 S_old_path = "/gpfs0/kats/users/wunch/semisup_data/sig"
 S_new_path = "/gpfs0/kats/users/wunch/semisup_evs/sig_rinv_0.50_mjj_500_gen_ptcut_rem_mjjcut"
-
 j1_old_df, j2_old_df = load_data_old(S_old_path, n_ev=10000)
 j1_new_df, j2_new_df = load_data_new(S_new_path, n_ev=10000)
-
-mult1_old = j1_old_df.mult
-mult1_new = j1_new_df.mult
-
-plt.figure()
-plt.hist([mult1_old, mult1_new],
-         bins=np.arange(min(mult1_old.min(), mult1_new.min()), max(mult1_old.max(), mult1_new.max())+2), align='left',
-         density=True, histtype='step')
-plt.legend(['old', 'new'])
-plt.savefig('sig_new_vs_old.png')
+mult1_sig_old = j1_old_df.mult
+mult1_sig_new = j1_new_df.mult
 
 B_old_path = "/gpfs0/kats/users/wunch/semisup_data/bkg"
 B_new_path = "/gpfs0/kats/users/wunch/semisup_evs/bkg"
-
 j1_old_df, j2_old_df = load_data_old(B_old_path, n_ev=10000)
 j1_new_df, j2_new_df = load_data_new(B_new_path, n_ev=10000)
-
-mult1_old = j1_old_df.mult
-mult1_new = j1_new_df.mult
+mult1_bkg_old = j1_old_df.mult
+mult1_bkg_new = j1_new_df.mult
 
 plt.figure()
-plt.hist([mult1_old, mult1_new],
-         bins=np.arange(min(mult1_old.min(), mult1_new.min()), max(mult1_old.max(), mult1_new.max())+2), align='left',
-         density=True, histtype='step')
-plt.legend(['old', 'new'])
-plt.savefig('bkg_new_vs_old.png')
+plt.hist([mult1_sig_old, mult1_bkg_old],
+         bins=np.arange(min(mult1_sig_old.min(), mult1_bkg_old.min()), max(mult1_sig_old.max(), mult1_bkg_old.max())+2),
+         align='left', density=True, histtype='step')
+plt.legend(['sig', 'bkg'])
+plt.savefig('sig_vs_bkg_old.png')
+
+plt.figure()
+plt.hist([mult1_sig_new, mult1_bkg_new],
+         bins=np.arange(min(mult1_sig_new.min(), mult1_bkg_new.min()), max(mult1_sig_new.max(), mult1_bkg_new.max())+2),
+         align='left', density=True, histtype='step')
+plt.legend(['sig', 'bkg'])
+plt.savefig('sig_vs_bkg_new.png')
