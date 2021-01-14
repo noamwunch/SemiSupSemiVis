fd46561f0af6e2e0efa0fb6a4bc5259d02de2eda
Noam Wunch <noamwunch1@gmail.com>
2021-01-14 16:29:47 +0200

    ?

--- test.py
+++ test.py
@@ -30,7 +30,7 @@ j1_model_save_path = exp_dir_path + f'j1_0/'
 j2_model_save_path = exp_dir_path + f'j2_0/'
 
 sig_frac = 0.5
-N = 5000
+N = 10000
 mask = -10.0
 n_constits = 80
 feats = ["constit_relPT", "constit_relEta", "constit_relPhi",
