152918db26c30f6138827bbdfc836ef7466df6d0
Noam Wunch <noamwunch1@gmail.com>
2021-01-25 20:51:35 +0200

    ?

--- test_feats_new_GenPtGt70.py
+++ test_feats_new_GenPtGt70.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import sys
 
 import numpy as np
 from matplotlib import pyplot as plt
@@ -12,16 +13,32 @@ from UTILS.plots_and_logs import plot_rocs, plot_mult, plot_learn_curve
 output_path = "./RESULTS/fullsup/rinv0.25sf0.20/"
 B_path_test = "/gpfs0/kats/users/wunch/semisup_evs/bkg/test"
 S_path_test = "/gpfs0/kats/users/wunch/semisup_evs/sig_rinv_0.25_mjj_500/test"
-B_path = "/gpfs0/kats/users/wunch/semisup_evs/bkg/train"
+B_path = "/gpfs0/kats/users/wunch/semisup_evs/bkg_GenPtGt70"
 S_path = "/gpfs0/kats/users/wunch/semisup_evs/sig_rinv_0.25_mjj_500/train"
 Path(output_path).mkdir(parents=True, exist_ok=True)
 
 N = 100000
 sig_frac = 0.5
 
+# TO ERASE
+N = int(sys.argv[1])
+sig_frac = 0.0
+pt_min = 100
+# TO ERASE
+
 print('Loading data...')
 j1_dat, j2_dat, label = combine_SB(B_path, S_path, N, sig_frac)
 print(f'Loaded  data: {len(label)} training examples \n')
+
+# TO ERASE
+print(f'Cutting on jet PT (both jet pt > {pt_min})..')
+valid = (j1_dat.jet_PT>pt_min) & (j2_dat.jet_PT>pt_min)
+j1_dat, j2_dat = j1_dat.loc[valid], j2_dat.loc[valid]
+label = label[valid]
+print(f'Cut on jet PT left with {np.sum(valid)} events')
+print(f'{sum(label)} signal events and {sum(~label.astype(bool))} background events')
+print(f'In total: {np.sum(valid)/len(valid):.2f} of background has both jetpt > {pt_min}')
+# TO ERASE
 
 # # Before preprocessing
 # bkg1, bkg2 = j1_dat.iloc[~label.astype(bool)], j2_dat.iloc[~label.astype(bool)]
