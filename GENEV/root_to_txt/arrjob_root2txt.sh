5307e9f44f8e3f8949be8403c56e276fa1d88321
Noam Wunch <noamwunch1@gmail.com>
2021-01-30 13:13:30 +0200

    ?

--- GENEV/root_to_txt/arrjob_root2txt.sh
+++ GENEV/root_to_txt/arrjob_root2txt.sh
@@ -8,12 +8,12 @@
 #$ -e ./stderr.txt
 #$ -o ./stdout.txt
 
-#$ -t 41-60
+#$ -t 1-20
 
 root_macro_dir=$(pwd)
 root_program_dir=/gpfs0/kats/projects/Delphes-3.4.2
 
-root_file_dir="/gpfs0/kats/users/wunch/semisup_evs/sig_rinv0.25_mZp1000_GenMjjGt400_GenPtGt40_GenEtaSt3_MjjGt500_PtGt50_EtaSt2.5/train"
+root_file_dir="/gpfs0/kats/users/wunch/semisup_evs/sig_rinv0.25_mZp1000_GenMjjGt400_GenPtGt40_GenEtaSt3_MjjGt500_PtGt50_EtaSt2.5/test"
 
 inp_file="$root_file_dir/$SGE_TASK_ID.root"
 
