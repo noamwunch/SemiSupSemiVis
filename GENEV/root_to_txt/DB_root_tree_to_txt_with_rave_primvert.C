780aa3096684ecc068244cd12dae0e5f886d9d3e
Noam Wunch <noamwunch1@gmail.com>
2021-03-02 13:39:32 +0200

    ?

--- GENEV/root_to_txt/DB_root_tree_to_txt_with_rave_primvert.C
+++ GENEV/root_to_txt/DB_root_tree_to_txt_with_rave_primvert.C
@@ -420,7 +420,7 @@ void DB_root_tree_to_txt_with_rave_primvert(const char *inputFile,
         vector < std::pair < float, rave::Track > > tracks;
 
         myfile << "Jet-number D0 Chi-squared Multiplicity type(4=vertex)" << endl;
-        vector <rave::Vertex> j1_vertices = factory.create(notinj1_tracks, j1_tracks); // Reconstruct vertices
+        vector <rave::Vertex> j1_vertices = factory.create(notinj1_tracks, j1_tracks, use_beamspot=true); // Reconstruct vertices
 
         // remove
         double vertexed_track_mult = 0;
@@ -482,7 +482,7 @@ void DB_root_tree_to_txt_with_rave_primvert(const char *inputFile,
         cout << " --------------------------------------------------------------- " << endl << endl << endl;
         // remove
 
-        vector <rave::Vertex> j2_vertices = factory.create(notinj2_tracks, j2_tracks); // Reconstruct vertices
+        vector <rave::Vertex> j2_vertices = factory.create(notinj2_tracks, j2_tracks, use_beamspot=true); // Reconstruct vertices
         for (vector<rave::Vertex>::const_iterator r = j2_vertices.begin(); r != j2_vertices.end(); ++r) {
             // Extract vertex info
             xp = (*r).position().x() * 10; //Converting to mm (RAVE produces output in cm)
