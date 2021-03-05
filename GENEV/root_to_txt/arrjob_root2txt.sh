eab120a8d59612b6de8a54fb23256127371495af
Noam Wunch <noamwunch1@gmail.com>
2021-03-05 11:24:04 +0200

    ?

--- GENEV/root_to_txt/arrjob_root2txt.sh
+++ GENEV/root_to_txt/arrjob_root2txt.sh
@@ -14,10 +14,10 @@ root_macro_dir=$(pwd)
 root_macro="root_tree_to_txt_with_rave_primvert"
 root_program_dir=/gpfs0/kats/projects/Delphes-3.4.2
 
-#root_inp_file_dir="/gpfs0/kats/users/wunch/semisup_evs/bkg_bb_GenMjjGt400_GenPtGt40_GenEtaSt3_MjjGt500_PtGt50_EtaSt2.5/test"
-#txt_out_file_dir="/gpfs0/kats/users/wunch/SemiSupSemiVis/test_rave3/bkg_bb_GenMjjGt400_GenPtGt40_GenEtaSt3_MjjGt500_PtGt50_EtaSt2.5"
-root_inp_file_dir="/gpfs0/kats/users/wunch/semisup_evs/sig_rinv0.25_mZp1000_GenMjjGt400_GenPtGt40_GenEtaSt3_MjjGt500_PtGt50_EtaSt2.5/test/"
-txt_out_file_dir="/gpfs0/kats/users/wunch/SemiSupSemiVis/test_vertexing/sig_rinv0.25_mZp1000_GenMjjGt400_GenPtGt40_GenEtaSt3_MjjGt500_PtGt50_EtaSt2.5"
+root_inp_file_dir="/gpfs0/kats/users/wunch/semisup_evs/bkg_bb_GenMjjGt400_GenPtGt40_GenEtaSt3_MjjGt500_PtGt50_EtaSt2.5/test"
+txt_out_file_dir="/gpfs0/kats/users/wunch/SemiSupSemiVis/test_vertexing/bkg_bb_GenMjjGt400_GenPtGt40_GenEtaSt3_MjjGt500_PtGt50_EtaSt2.5"
+#root_inp_file_dir="/gpfs0/kats/users/wunch/semisup_evs/sig_rinv0.25_mZp1000_GenMjjGt400_GenPtGt40_GenEtaSt3_MjjGt500_PtGt50_EtaSt2.5/test/"
+#txt_out_file_dir="/gpfs0/kats/users/wunch/SemiSupSemiVis/test_vertexing/sig_rinv0.25_mZp1000_GenMjjGt400_GenPtGt40_GenEtaSt3_MjjGt500_PtGt50_EtaSt2.5"
 inp_file="$root_inp_file_dir/$SGE_TASK_ID.root"
 out_file="$txt_out_file_dir/$SGE_TASK_ID.root.txt"
 
--- UTILS/utils.py
+++ UTILS/utils.py
@@ -268,6 +268,161 @@ def evs_txt2jets_df_with_verts(events_dir_path, n_ev=None, sort="PT", get_verts=
 
     return jets1_df, jets2_df
 
+def evs_txt2jets_df_with_verts2(events_dir_path, n_ev=None, sort="PT", get_verts=False):
+    """Takes event list path (string) and returns a pandas Dataframe with jet info"""
+    def jet_list2jet_df(jets_list, get_verts=False):
+        event_columns = ["Event", "MET", "Mjj"]
+        jet_columns = ["Jet", "jet_PT", "jet_Eta", "jet_Phi", "dR_closest_parton"]
+        constituent_columns = ["constit_PT", "constit_Eta", "constit_Phi",
+                               "constit_type", "constit_PID", "constit_D0", "constit_DZ", "abs_D0"]
+        vertex_columns = ["verts_D0", "verts_Dz", "verts_chisq", "verts_mult"]
+
+        # Create DataFrame
+        if get_verts:
+            columns = event_columns + jet_columns + constituent_columns + vertex_columns
+        else:
+            columns = event_columns + jet_columns + constituent_columns
+
+        jets_df = pd.DataFrame(jets_list, columns=columns)
+
+        # Convert list columns to np.array columns
+        if get_verts:
+            list_cols = constituent_columns + vertex_columns
+        else:
+            list_cols = constituent_columns
+
+        jets_df[list_cols] = jets_df[list_cols].applymap(lambda x: np.array(x))
+
+        # Assign dtypes to non list columns
+        dtypes = {"Event": np.int, "MET": np.float, "Mjj": np.float,
+                  "Jet": np.int, "jet_PT": np.float, "jet_Eta": np.float, "jet_Phi": np.float,
+                  "dR_closest_parton": np.float}
+        jets_df = jets_df.astype(dtypes)
+
+        # Number of vertices in jet
+        none_mask = jets_df.verts_D0.map(lambda x: x is None)
+        jets_df["n_verts"] = jets_df.verts_D0.map(lambda x: np.size(x))
+        jets_df.loc[none_mask, 'n_verts'] = 0
+
+        # Derive other useful columns
+        jets_df['mult'] = jets_df.constit_PT.map(lambda x: len(x))
+        jets_df['constit_relPT'] = jets_df.constit_PT/jets_df.jet_PT
+        jets_df['constit_relPhi'] = jets_df.constit_Phi - jets_df.jet_Phi
+        jets_df.constit_relPhi = jets_df.constit_relPhi.map(lambda phi: phi - 2*np.pi * (phi>np.pi) + 2*np.pi * (phi<-np.pi))
+        jets_df['constit_relEta'] = jets_df.constit_Eta - jets_df.jet_Eta
+        jets_df['constit_relDZ'] = jets_df['constit_DZ'] / np.cosh(jets_df['jet_Eta'])
+        jets_df['constit_deltaR'] = np.power(np.power(jets_df.constit_relPhi, 2.0) +
+                                             np.power(jets_df.constit_relEta, 2.0), 0.5)
+
+        return jets_df
+
+    # Redefine sort variable to correspond to column index
+    if sort == "PT":
+        sort = 0
+    elif sort == "D0":
+        sort = 7
+    # Initialize jet list
+    jets1_list = []
+    jets2_list = []
+    # Initialize temp jet info
+    jet1_info = []
+    jet2_info = []
+    jet1_constits = []
+    jet2_constits = []
+    jet1_verts = []
+    jet2_verts = []
+
+    # Loop over txt file paths in events_paths
+    ev_num = 0
+    pathlist = Path(events_dir_path).glob('**/*root.txt')
+    for events_path in pathlist:
+        if n_ev is not None:
+            if ev_num >= n_ev:
+                break
+        # Loop over lines in txt file
+        with open(str(events_path)) as events:
+            for line in events:
+                row = line.split()
+
+                # New event
+                if row[0] == "--":
+                    # Log previuos event
+                    if ev_num > 0:
+                        event_info = [ev_num, met, mjj]
+
+                        # Append jet-1 info
+                        jet1_constits = np.array(jet1_constits, dtype="float")
+                        if sort is not None:
+                            jet1_constits = jet1_constits[jet1_constits[:, sort].argsort()[::-1]]
+
+                        if get_verts:
+                            jet1_verts = np.array(jet1_verts, dtype="float")
+                            j1_row_list =  event_info + jet1_info + list(jet1_constits.T) + list(jet1_verts.T)
+                        else:
+                            j1_row_list = event_info + jet1_info + list(jet1_constits.T)
+
+                        jets1_list.append(j1_row_list)
+
+                        # Append jet-2 info
+                        jet2_constits = np.array(jet2_constits, dtype="float")
+                        if sort is not None:
+                            jet2_constits = jet2_constits[jet2_constits[:, sort].argsort()[::-1]]
+
+                        if get_verts:
+                            jet2_verts = np.array(jet2_verts, dtype="float")
+                            j2_row_list = event_info + jet2_info + list(jet2_constits.T) + list(jet2_verts.T)
+                        else:
+                            j2_row_list = event_info + jet1_info + list(jet2_constits.T)
+
+                        jets2_list.append(j2_row_list)
+
+                    # Initialize for next event
+                    jet1_constits = []
+                    jet2_constits = []
+                    jet1_verts = []
+                    jet2_verts = []
+                    ev_num += 1
+                    if ev_num > n_ev:
+                        break
+                    continue
+
+                # General event info
+                if row[0] == "MET:":
+                    met = row[1]
+                if row[0] == "MJJ:":
+                    mjj = row[1]
+
+                # General jet info
+                if (row[0] == "Jet") and (row[1] == "1"):
+                    jet1_info = [row[1], row[3], row[5], row[7], row[9]]
+                    continue
+                if (row[0] == "Jet") and (row[1] == "2"):
+                    jet2_info = [row[1], row[3], row[5], row[7], row[9]]
+                    continue
+
+                # Constituents info
+                if row[0].isdigit() and (int(row[4]) in [1, 2, 3]):
+                    if int(row[0]) == 1:
+                        jet1_constits.append(row[1:] + [abs(float(row[7]))])
+                        continue
+                    if int(row[0]) == 2:
+                        jet2_constits.append(row[1:] + [abs(float(row[7]))])
+                        continue
+
+                # Vertices info
+                if row[0].isdigit() and int(row[4])==4:
+                    if int(row[0]) == 1:
+                        jet1_verts.append(row[1:-1])
+                        continue
+                    if int(row[0]) == 2:
+                        jet2_verts.append(row[1:-1])
+                        continue
+
+    jets1_df = jet_list2jet_df(jets1_list, get_verts=get_verts)
+    jets2_df = jet_list2jet_df(jets2_list, get_verts=get_verts)
+
+    return jets1_df, jets2_df
+
 def create_one_hot_encoder(class_dict):
     enc = OneHotEncoder()
     unique_class = np.unique(list(class_dict.keys()))
