32d353ffe35bc94b6c77559473b654693531cd49
Noam Wunch <noamwunch1@gmail.com>
2021-03-21 12:00:33 +0200

    ?

--- RUN/run_sge_from_argseed.sh
+++ RUN/run_sge_from_argseed.sh
@@ -10,6 +10,6 @@
 source $HOME/.bash_profile
 conda activate ML
 
-awk '{print $3}' argseed_sf0.025.txt | xargs python ../semisup.py
+awk '{print $3}' argseed_lowmass.txt | xargs python ../semisup.py
 
 conda deactivate
