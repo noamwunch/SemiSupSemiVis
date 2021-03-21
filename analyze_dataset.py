9ae13ce34af469a661a872ff6e7e30ed347bc1ec
Noam Wunch <noamwunch1@gmail.com>
2021-03-21 17:49:57 +0200

    ?

--- analyze_dataset.py
+++ analyze_dataset.py
@@ -31,7 +31,7 @@ fig_format = '.pdf'
 Path(plot_path).mkdir(parents=True, exist_ok=True)
 
 ## Distributions
-distibutions = True
+distibutions = False
 B_path_dist = "/gpfs0/kats/users/wunch/semisup_dataset/bkg_bb_GenMjjGt400_GenPtGt40_GenEtaSt3_MjjGt500_PtGt50_EtaSt2.5/train"
 S_path_dist = "/gpfs0/kats/users/wunch/semisup_dataset/sig_dl0.5_rinv0.20_mZp1250_lambda20_GenMjjGt400_GenPtGt40_GenEtaSt3_MjjGt500_PtGt50_EtaSt2.5/train"
 Ndists = 100000
@@ -48,7 +48,7 @@ dat_eff_metcut = 1e-2
 dat_eff_nncut = 2e-3
 
 # NN
-NN = True
+NN = False
 mask = -10.0
 n_constits = 80
 feats, n_cols = determine_feats(with_displacement='True',
@@ -143,14 +143,33 @@ if bumphunt:
     thresh = np.quantile(mult, 1-dat_eff_metcut)
     valid = mult>thresh
 
-    sigeff = np.sum(valid & sig_mask)/np.sum(sig_mask)
-    bkgeff = np.sum(valid & bkg_mask)/np.sum(bkg_mask)
-    sig_frac_post = np.sum(valid & sig_mask)/np.sum(valid)
-    Npost = np.sum(valid)
-    txt = f'Signal fraction (before cut, after cut):\n({sig_frac}, {sig_frac_post:.3f})' \
-          f'\n\nTotal events (before cut, after cut):\n({Ntest}, {Npost})' \
-          f'\n\nSignal efficiency of cut:\n{sigeff:.2f}' \
-          f'\n\nBackground efficiency of cut:\n{bkgeff:.2e}'
+    # No cut entire region
+    entreg_both_nocut = np.sum(all_mask)
+    entreg_sig_nocut = np.sum(sig_mask)
+    entreg_bkg_nocut = np.sum(bkg_mask)
+    # No cut signal region
+    sigreg_both_nocut = np.sum(sig_reg_mask)
+    sigreg_sig_nocut = np.sum(sig_reg_mask & sig_mask)
+    sigreg_bkg_nocut = np.sum(sig_reg_mask & bkg_mask)
+    # NN cut entire region
+    entreg_both_multcut = np.sum(valid)
+    entreg_sig_multcut = np.sum(sig_mask & valid)
+    entreg_bkg_multcut = np.sum(bkg_mask & valid)
+    # NN cut signal region
+    sigreg_both_multcut = np.sum(sig_reg_mask & valid)
+    sigreg_sig_multcut = np.sum(sig_reg_mask & sig_mask & valid)
+    sigreg_bkg_multcut = np.sum(sig_reg_mask & bkg_mask & valid)
+    txt = f'(signal, background, total)' \
+          f'\n______________________________' \
+          f'\n\nEntire region no-cut\n({entreg_sig_nocut}, {entreg_bkg_nocut}, {entreg_both_nocut})' \
+          f'\n\nSignal region no-cut\n({sigreg_sig_nocut}, {sigreg_bkg_nocut}, {sigreg_both_nocut})' \
+          f'\n\nEntire region mult-cut\n({entreg_sig_multcut}, {entreg_bkg_multcut}, {entreg_both_multcut})' \
+          f'\n\nSignal region mult-cut\n({sigreg_sig_multcut}, {sigreg_bkg_multcut}, {sigreg_both_multcut})'
+
+    # txt = f'Signal fraction (before cut, after cut):\n({sig_frac}, {sig_frac_post:.3f})' \
+    #       f'\n\nTotal events (before cut, after cut):\n({Ntest}, {Npost})' \
+    #       f'\n\nSignal efficiency of cut:\n{sigeff:.2f}' \
+    #       f'\n\nBackground efficiency of cut:\n{bkgeff:.2e}'
 
     plt.figure()
     plt.hist([mjj, mjj.loc[valid]], label=['before multiplicity cut', 'after multiplicity cut'], **hist_dict)
@@ -212,11 +231,6 @@ if bumphunt:
         thresh = np.quantile(pred, 1-dat_eff_nncut)
         valid = pred>thresh
 
-        sigeff = np.sum(valid & sig_mask)/np.sum(sig_mask)
-        bkgeff = np.sum(valid & bkg_mask)/np.sum(bkg_mask)
-        sig_frac_post = np.sum(valid & sig_mask)/np.sum(valid)
-        Npost = np.sum(valid)
-
         # No cut entire region
         entreg_both_nocut = np.sum(all_mask)
         entreg_sig_nocut = np.sum(sig_mask)
@@ -240,11 +254,6 @@ if bumphunt:
               f'\n\nSignal region no-cut\n({sigreg_sig_nocut}, {sigreg_bkg_nocut}, {sigreg_both_nocut})' \
               f'\n\nEntire region nn-cut\n({entreg_sig_nncut}, {entreg_bkg_nncut}, {entreg_both_nncut})' \
               f'\n\nSignal region nn-cut\n({sigreg_sig_nncut}, {sigreg_bkg_nncut}, {sigreg_both_nncut})'
-
-        # txt = f'Signal fraction (before cut, after cut):\n({sig_frac}, {sig_frac_post:.3f})' \
-        #       f'\n\nTotal events (before cut, after cut):\n({Ntest}, {Npost})' \
-        #       f'\n\nSignal efficiency of cut:\n{sigeff:.2f}' \
-        #       f'\n\nBackground efficiency of cut:\n{bkgeff:.2e}'
 
         plt.figure()
         plt.hist([mjj, mjj.loc[valid]], label=['before nn cut', 'after nn cut'], **hist_dict)
