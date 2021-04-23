bfc0d16dd596658fe90b9eb725bab51f55d171f0
Noam Wunch <noamwunch1@gmail.com>
2021-04-23 10:20:05 +0300

    ?

--- test_rapidity_cut.py
+++ test_rapidity_cut.py
@@ -4,7 +4,7 @@ from matplotlib import pyplot as plt
 
 B_path = "/gpfs0/kats/users/wunch/semisup_dataset/bkg_bb_GenMjjGt400_GenPtGt40_GenEtaSt3_MjjGt500_PtGt50_EtaSt2.5/test"
 S_path = "/gpfs0/kats/users/wunch/semisup_dataset/sig_dl0.5_rinv0.00_mZp1500_lambda20_GenMjjGt600_GenPtGt40_GenEtaSt3_MjjGt800_PtGt50_EtaSt2.5/test"
-N = 5000
+N = 10000
 
 j1_bkg, j2_bkg, _ = combine_SB(B_path, S_path, N, 0)
 j1_sig, j2_sig, _ = combine_SB(B_path, S_path, N, 1)
@@ -23,8 +23,10 @@ plt.savefig('y_hist_new')
 y_bkg = j1_bkg.Mjj
 y_sig = j1_sig.Mjj
 
+bins = np.arange(1000, 3000, 40)
+
 plt.figure()
-plt.hist([y_sig, y_bkg], histtype='step', density=True, label=["Dark events", "QCD events"])
+plt.hist([y_sig, y_bkg], histtype='step', density=True, label=["Dark events", "QCD events"], align='center', bins=bins)
 plt.legend()
 plt.xlabel('$M_{jj}$')
 plt.savefig('mjj_hist')
