2577d3ad48282ea97fdeb6cb227a2c49423a8e11
Noam Wunch <noamwunch1@gmail.com>
2021-04-23 11:09:06 +0300

    ?

--- test_rapidity_cut.py
+++ test_rapidity_cut.py
@@ -7,6 +7,7 @@ S_path = "/gpfs0/kats/users/wunch/semisup_dataset/sig_dl0.5_rinv0.00_mZp1500_lam
 
 N_bkg = 200000
 N_sig = 1000
+sig_region = (1200, 1500)
 
 j1_bkg, j2_bkg, _ = combine_SB(B_path, S_path, N_bkg, 0)
 j1_sig, j2_sig, _ = combine_SB(B_path, S_path, N_sig, 1)
@@ -25,11 +26,25 @@ plt.savefig('y_hist_new')
 y_bkg = j1_bkg.Mjj
 y_sig = j1_sig.Mjj
 
+N_sig_reg = np.sum(y_sig.between(*sig_region))
+N_bkg_reg = np.sum(y_bkg.between(*sig_region))
+sig_frac = N_sig_reg/N_bkg_reg
+significance = N_sig_reg/(np.sqrt(N_sig_reg + N_bkg_reg))
+
 bins = np.arange(1000, 3000, 40)
+hist_dict = dict(histtype='step', align='mid')
+txt_dict = dict(xy=(0.6, 0.7), xycoords='axes fraction')
+labels = [f"{N_sig} Dark events", f"{N_bkg} QCD events"]
+
+txt_sigfrac = f"signal-fraction: {N_sig/N_bkg:.2f}"
+txt_significance = "significance: %.2f $\\sigma$" %(significance)
+txt = f"In signal region (1200-1500 GeV): \n{txt_sigfrac} \n{txt_significance}"
 
 plt.figure()
-plt.hist([y_sig, y_bkg], histtype='step', density=True, label=["Dark events", "QCD events"], align='mid', bins=bins)
+plt.hist([y_sig, y_bkg], bins=bins, label=labels, **hist_dict)
+plt.txt(txt, **txt_dict)
 plt.legend()
+plt.ylabel('Events/(40 GeV)')
 plt.xlabel('$M_{jj}$')
 plt.savefig('mjj_hist')
 
