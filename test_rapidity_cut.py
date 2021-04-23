84eb4db56d74bed0088b6b99f990429c61bc8943
Noam Wunch <noamwunch1@gmail.com>
2021-04-23 11:50:59 +0300

    ?

--- test_rapidity_cut.py
+++ test_rapidity_cut.py
@@ -3,11 +3,12 @@ import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 
-B_path = "/gpfs0/kats/users/wunch/semisup_dataset/bkg_bb_GenMjjGt400_GenPtGt40_GenEtaSt3_MjjGt500_PtGt50_EtaSt2.5/test"
+B_path = "/gpfs0/kats/users/wunch/semisup_dataset/bkg_bb_GenMjjGt400_GenPtGt40_GenEtaSt3_MjjGt500_PtGt50_EtaSt2.5/train"
 S_path = "/gpfs0/kats/users/wunch/semisup_dataset/sig_dl0.5_rinv0.00_mZp1500_lambda20_GenMjjGt600_GenPtGt40_GenEtaSt3_MjjGt800_PtGt50_EtaSt2.5/test"
 
-N_bkg = 200000
-N_sig = 200
+N_bkg = 40000
+N_sig = 2000
+tot_region = (1000, 3000)
 sig_region = (1200, 1500)
 
 j1_bkg, j2_bkg, _ = combine_SB(B_path, S_path, N_bkg, 0)
@@ -27,13 +28,16 @@ plt.savefig('y_hist_new')
 y_bkg = j1_bkg.Mjj
 y_sig = j1_sig.Mjj
 y_both = pd.concat([j1_bkg.Mjj, j1_sig.Mjj])
+bin_size = 40  # GeV
 
+N_sig_tot = np.sum(y_sig.between(*tot_region))
+N_bkg_tot = np.sum(y_bkg.between(*tot_region))
 N_sig_reg = np.sum(y_sig.between(*sig_region))
 N_bkg_reg = np.sum(y_bkg.between(*sig_region))
 sig_frac = N_sig_reg/N_bkg_reg
 significance = N_sig_reg/(np.sqrt(N_sig_reg + N_bkg_reg))
 
-bins = np.arange(1000, 3000, 40)
+bins = np.arange(tot_region[0], tot_region[1], bin_size)
 hist_dict = dict(histtype='step', align='mid')
 labels = ["Dark events", "QCD events", "QCD + Dark events"]
 
@@ -43,7 +47,7 @@ txt_sigfrac = f"signal-fraction: {sig_frac:.2f}"
 txt_significance = "significance: %.2f $\\sigma$" %(significance)
 txt_reg = f"In signal region (1200-1500 GeV): \n{txt_Nreg} \n{txt_sigfrac} \n{txt_significance}"
 
-txt_Ntot = f"(QCD events, Dark events): ({N_bkg}, {N_sig})"
+txt_Ntot = f"(QCD events, Dark events): ({N_bkg_tot}, {N_sig_tot})"
 txt_tot = f"In entire region (1000-3000 GeV): \n{txt_Ntot}"
 
 # plot
