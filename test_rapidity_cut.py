ac4edbf27dcc6f75bfa99b25ff6457cc553c4cde
Noam Wunch <noamwunch1@gmail.com>
2021-04-23 10:01:31 +0300

    ?

--- test_rapidity_cut.py
+++ test_rapidity_cut.py
@@ -3,8 +3,8 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 B_path = "/gpfs0/kats/users/wunch/semisup_dataset/bkg_bb_GenMjjGt400_GenPtGt40_GenEtaSt3_MjjGt500_PtGt50_EtaSt2.5/test"
-S_path = "/gpfs0/kats/users/wunch/semisup_dataset/sig_dl0.5_rinv0.20_mZp1250_lambda20_GenMjjGt400_GenPtGt40_GenEtaSt3_MjjGt500_PtGt50_EtaSt2.5/test"
-N = 50000
+S_path = "/gpfs0/kats/users/wunch/semisup_dataset/sig_dl0.5_rinv0.00_mZp1500_lambda20_GenMjjGt600_GenPtGt40_GenEtaSt3_MjjGt800_PtGt50_EtaSt2.5/test"
+N = 5000
 
 j1_bkg, j2_bkg, _ = combine_SB(B_path, S_path, N, 0)
 j1_sig, j2_sig, _ = combine_SB(B_path, S_path, N, 1)
@@ -15,4 +15,4 @@ y_sig = np.abs((j1_sig.jet_Eta - j2_sig.jet_Eta)/2)
 plt.hist([y_sig, y_bkg], histtype='step', density=True, label=["Dark events", "QCD events"])
 plt.legend()
 plt.xlabel('y')
-plt.savefig('y_hist')
+plt.savefig('y_hist_new')
