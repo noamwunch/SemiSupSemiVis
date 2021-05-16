b3de80add5ff40a283abc336c676cb0a316f78ff
Noam Wunch <noamwunch1@gmail.com>
2021-05-16 17:55:46 +0300

    ?

--- test_fullysup_highlevel.py
+++ test_fullysup_highlevel.py
@@ -40,7 +40,8 @@ def preproc_for_dense(j_inp):
     med_dz = med_dz * 5
     med_d0 = med_d0 * 5
 
-    dense_inp = np.hstack([mult, n_verts, fake_thrust, med_dz, med_d0])
+    dense_inp = np.stack((mult, n_verts, fake_thrust, med_dz, med_d0), axis=1)
+    print(f'dense_inp.shape = {dense_inp.shape}')
 
     return dense_inp
 
