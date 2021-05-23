556bb18ecea9f53906b6082dfb23932a60b9ddfb
Noam Wunch <noamwunch1@gmail.com>
2021-05-23 08:25:25 +0300

    ?

--- test_higlelevel_feats_new.py
+++ test_higlelevel_feats_new.py
@@ -35,6 +35,7 @@ Btest_path = "/gpfs0/kats/users/wunch/semisup_dataset/bkg_bb_GenMjjGt800_GenPtGt
 Stest_path = "/gpfs0/kats/users/wunch/semisup_dataset/sig_dl0.5_rinv0.00_mZp1500_lambda20_GenMjjGt800_GenPtGt40_GenEtaSt3_MjjGt1000_PtGt50_EtaSt2.5_y*lt1/test"
 
 Ntest = 2e4
+feats=('constit_mult', 'ptwmean_dR', 'ptwmean_absD0', 'ptwmean_absDZ', 'c1b')
 
 print('Loading train data...')
 j1_df, j2_df, event_labels = combine_SB(Btest_path, Stest_path, Ntest, 0.5)
@@ -51,17 +52,16 @@ print('Finished preprocessing events')
 
 print('Plotting preproced event histograms')
 pdf_path = 'event_hists_preproc.pdf'
-plot_preproced_feats_dense(j1_preproc, j2_preproc, event_labels=event_labels, pdf_path=pdf_path)
+plot_preproced_feats_dense(j1_preproc, j2_preproc, event_labels=event_labels, feats=feats, pdf_path=pdf_path)
 print('Finished plotting preproced event histograms')
 
-preproc_args = dict(feats=('constit_mult', 'ptwmean_dR', 'ptwmean_absD0', 'ptwmean_absDZ', 'c1b'))
 print('Plotting preproced event histograms jet 1')
 pdf_path = 'event_hists_preproc_jet1.pdf'
-plot_nn_inp_histograms_dense(j1_preproc, event_labels=event_labels, preproc_args=preproc_args, pdf_path=pdf_path)
+plot_nn_inp_histograms_dense(j1_preproc, event_labels=event_labels, feats=feats, pdf_path=pdf_path)
 print('Finished plotting preproced event histograms jet 1')
 
 print('Plotting preproced event histograms jet 2')
 pdf_path = 'event_hists_preproc_jet2.pdf'
-plot_nn_inp_histograms_dense(j1_preproc, event_labels=event_labels, preproc_args=preproc_args, pdf_path=pdf_path)
+plot_nn_inp_histograms_dense(j1_preproc, event_labels=event_labels, feats=feats, pdf_path=pdf_path)
 print('Finished plotting preproced event histograms jet 2')
 
