65a3c4ae90396f854d637644c54df3a245dbe408
Noam Wunch <noamwunch1@gmail.com>
2021-05-25 15:29:48 +0300

    ?

--- UTILS/dense_classifier.py
+++ UTILS/dense_classifier.py
@@ -100,7 +100,7 @@ def calc_photonE_over_bothE(col_dict):
 
 def create_dense_classifier(nfeats, log=''):
     dropout = 0.2
-    lr = 0.1
+    lr = 0.05
     # decay params
     decay = True
     N_train = int(1e5)
