b16c7bf6006c19210ec15fcab4b8d80e2bbe4c1f
Noam Wunch <noamwunch1@gmail.com>
2021-06-04 19:09:26 +0300

    ?

--- test_higlelevel_feats_new.py
+++ test_higlelevel_feats_new.py
@@ -91,7 +91,7 @@ S_path = "/gpfs0/kats/users/wunch/semisup_dataset/sig_dl0.5_rinv0.00_mZp1500_lam
 Btest_path = "/gpfs0/kats/users/wunch/semisup_dataset/bkg_bb_GenMjjGt800_GenPtGt40_GenEtaSt3_MjjGt1000_PtGt50_EtaSt2.5_y*lt1/test"
 Stest_path = "/gpfs0/kats/users/wunch/semisup_dataset/sig_dl0.5_rinv0.00_mZp1500_lambda20_GenMjjGt800_GenPtGt40_GenEtaSt3_MjjGt1000_PtGt50_EtaSt2.5_y*lt1/test"
 
-Ntest = 2e3
+Ntest = 2e4
 feats = ['constit_mult',
          'vert_count',
          'ptwmean_dR',
