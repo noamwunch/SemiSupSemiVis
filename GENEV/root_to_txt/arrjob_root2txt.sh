c3d3fc44e524f8053018770f5af73bad168521c5
Noam Wunch <noamwunch1@gmail.com>
2021-06-05 12:52:37 +0300

    ?

--- GENEV/root_to_txt/arrjob_root2txt.sh
+++ GENEV/root_to_txt/arrjob_root2txt.sh
@@ -34,6 +34,6 @@ mkdir -p $txt_out_file_dir
 source $HOME/.bash_profile
 cd $root_program_dir
 root -b << EOF
-.x $root_macro_dir/$root_macro.C("$inp_file", $dijet, $veto_isolep, $PT_min, $PT_max, $Eta_min, $Eta_max, $Mjj_min, $Mjj_max, $ystar_min, $dR_jet,"$out_file")
+.x $root_macro_dir/$root_macro.C("$inp_file", $dijet, $veto_isolep, $PT_min, $PT_max, $Eta_min, $Eta_max, $Mjj_min, $Mjj_max, $ystar_max, $dR_jet,"$out_file")
 EOF
 
---
+++ plot_rocs.py
@@ -0,0 +1,27 @@
+from UTILS.plots_and_logs import plot_rocs_significance
+import numpy as np
+
+exp_dir_path = "../RESULTS/05062021_sf0.005_40eps_1e5evs_forseminar_new_withsig/"
+
+classifier_preds_save_dir = exp_dir_path + 'classifier_preds/'
+# for classifier_name, classifier_dict in zip(classifier_dicts.keys(), classifier_dicts.values()):
+#     probS = classifier_dict['probS']
+#     np.save(classifier_preds_save_dir+classifier_name+'.npy', probS)
+
+event_label_test = np.load(classifier_preds_save_dir+'event_labels.npy')
+
+event_semisup_probS = np.load(classifier_preds_save_dir+'event NN'+'.npy')
+event_unsup_probS = np.load(classifier_preds_save_dir+'event multiplicity'+'.npy')
+
+classifier_dicts = {'event NN': {'probS': event_semisup_probS, 'plot_dict': {'linestyle': '-', 'color': 'black'}},
+                    'event multiplicity': {'probS': event_unsup_probS, 'plot_dict': {'linestyle': '--', 'color': 'black'}}}
+
+plot_rocs_significance(classifier_dicts=classifier_dicts, true_lab=event_label_test,
+                       save_path=exp_dir_path+'log_ROC_significance_new.pdf')
+
+# classifier_dicts = {'event NN': {'probS': event_semisup_probS, 'plot_dict': {'linestyle': '-', 'color': 'black'}},
+#                     'j1 NN': {'probS': j1_semisup_probS, 'plot_dict': {'linestyle': '-', 'color': 'blue'}},
+#                     'j2 NN': {'probS': j2_semisup_probS, 'plot_dict': {'linestyle': '-', 'color': 'green'}},
+#                     'event multiplicity': {'probS': event_unsup_probS, 'plot_dict': {'linestyle': '--', 'color': 'black'}},
+#                     'j1 multiplicity': {'probS': j1_unsup_probS, 'plot_dict': {'linestyle': '--', 'color': 'blue'}},
+#                     'j2 multiplicity': {'probS': j2_unsup_probS, 'plot_dict': {'linestyle': '--', 'color': 'green'}}}
