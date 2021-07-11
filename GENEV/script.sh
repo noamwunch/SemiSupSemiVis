704df2d207fd808b4edfee1403ece693bbcbbe0b
Noam Wunch <noamwunch1@gmail.com>
2021-07-11 21:30:47 +0300

    ?

--- GENEV/script.sh
+++ GENEV/script.sh
@@ -4,13 +4,13 @@
 cd madgraph_and_pythia
 rm std*
 settings_madgraph="/gpfs0/kats/users/wunch/SemiSupSemiVis/GENEV/settings_madgraph/Settings_bb.txt"
-qsub -N genevs_cc -t 1:2 arrjob_genev.sh "$settings_madgraph"
+qsub -N genevs_bb -t 1:2 arrjob_genev.sh "$settings_madgraph"
 cd ..
 
 cd root_to_txt
 rm std*
 args_root="/gpfs0/kats/users/wunch/SemiSupSemiVis/GENEV/args_root/args_root_bb.sh"
-qsub -N root2txt_cc -t 1:2 -hold_jid genevs_cc arrjob_root2txt.sh "$args_root"
+qsub -N root2txt_bb -t 1:2 -hold_jid genevs_bb arrjob_root2txt.sh "$args_root"
 cd ..
 
 ###cc#####
@@ -30,24 +30,24 @@ cd ..
 cd madgraph_and_pythia
 rm std*
 settings_madgraph="/gpfs0/kats/users/wunch/SemiSupSemiVis/GENEV/settings_madgraph/Settings_dark_0.5mm_rinv0.txt"
-qsub -N genevs_cc -t 1:2 arrjob_genev.sh "$settings_madgraph"
+qsub -N genevs_dark_0.5mm_rinv0 -t 1:2 arrjob_genev.sh "$settings_madgraph"
 cd ..
 
 cd root_to_txt
 rm std*
 args_root="/gpfs0/kats/users/wunch/SemiSupSemiVis/GENEV/args_root/args_root_dark_0.5mm_rinv0.sh"
-qsub -N root2txt_cc -t 1:2 -hold_jid genevs_cc arrjob_root2txt.sh "$args_root"
+qsub -N root2txt_dark_0.5mm_rinv0 -t 1:2 -hold_jid genevs_dark_0.5mm_rinv0 arrjob_root2txt.sh "$args_root"
 cd ..
 
 ###dark_0.25mm_rinv0#####
 cd madgraph_and_pythia
 rm std*
 settings_madgraph="/gpfs0/kats/users/wunch/SemiSupSemiVis/GENEV/settings_madgraph/Settings_dark_0.25mm_rinv0.txt"
-qsub -N genevs_cc -t 1:2 arrjob_genev.sh "$settings_madgraph"
+qsub -N genevs_dark_0.25mm_rinv0 -t 1:2 arrjob_genev.sh "$settings_madgraph"
 cd ..
 
 cd root_to_txt
 rm std*
 args_root="/gpfs0/kats/users/wunch/SemiSupSemiVis/GENEV/args_root/args_root_dark_0.25mm_rinv0.sh"
-qsub -N root2txt_cc -t 1:2 -hold_jid genevs_cc arrjob_root2txt.sh "$args_root"
+qsub -N root2txt_dark_0.25mm_rinv0 -t 1:2 -hold_jid genevs_dark_0.25mm_rinv0 arrjob_root2txt.sh "$args_root"
 cd ..
