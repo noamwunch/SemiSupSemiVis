ab1514f03bfb174f235b2f75889c24f6a8d69588
Noam Wunch <noamwunch1@gmail.com>
2021-07-11 21:03:39 +0300

    ?

--- GENEV/script.sh
+++ GENEV/script.sh
@@ -3,11 +3,11 @@
 cd madgraph_and_pythia
 rm std*
 settings_madgraph="/gpfs0/kats/users/wunch/SemiSupSemiVis/GENEV/settings_madgraph/Settings_cc.txt"
-qsub -N genevs_cc -t 3:4 arrjob_genev.sh "$settings_madgraph"
+qsub -N genevs_cc -t 5:6 arrjob_genev.sh "$settings_madgraph"
 cd ..
 
 cd root_to_txt
 rm std*
-args_root="/gpfs0/kats/users/wunch/SemiSupSemiVis/GENEV/args_root/args_root_cc.txt"
-qsub -N root2txt_cc -t 3:4 -hold_jid genevs_cc arrjob_root2txt.sh "$args_root"
+args_root="/gpfs0/kats/users/wunch/SemiSupSemiVis/GENEV/args_root/args_root_cc.sh"
+qsub -N root2txt_cc -t 5:6 -hold_jid genevs_cc arrjob_root2txt.sh "$args_root"
 cd ..
