776a66266a9ac02532906b8e6f5200781cf74bce
Noam Wunch <noamwunch1@gmail.com>
2021-07-12 19:20:24 +0300

    ?

--- semisup_modular_vertcount.py
+++ semisup_modular_vertcount.py
@@ -376,10 +376,10 @@ def main_semisup(B_path, S_path, Btest_path, Stest_path, exp_dir_path, Ntrain=in
     with np.errstate(divide='ignore'):
         plot_rocs(classifier_dicts=classifier_dicts, true_lab=event_label_test,
                   save_path=exp_dir_path+'log_ROC.pdf')
-        plot_rocs(classifier_dicts=classifier_dicts2, true_lab=event_label_test,
-                  save_path=exp_dir_path+'log_ROC2.pdf')
-        plot_rocs_significance(classifier_dicts=classifier_dicts, true_lab=event_label_test,
-                               save_path=exp_dir_path+'log_ROC_significance.pdf')
+        # plot_rocs(classifier_dicts=classifier_dicts2, true_lab=event_label_test,
+        #           save_path=exp_dir_path+'log_ROC2.pdf')
+        # plot_rocs_significance(classifier_dicts=classifier_dicts, true_lab=event_label_test,
+        #                        save_path=exp_dir_path+'log_ROC_significance.pdf')
         # plot_rocs_significance(classifier_dicts=classifier_dict_mult, true_lab=event_label_test,
         #                        save_path=exp_dir_path+'log_ROC_mult_significance.pdf')
         # plot_rocs(classifier_dicts=classifier_dicts_weak, true_lab=weak_labels_test1,
